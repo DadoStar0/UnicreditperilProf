d4438af70ae757ea1bba16408d2b938ed29a0b78
Davide Simone <165477789+DadoStar0@users.noreply.github.com>
2024-05-18 17:56:13 +0200

    Add files via upload

--- pages/operazioni/aggiornaTotale.js
+++ pages/operazioni/aggiornaTotale.js
@@ -4,6 +4,8 @@ function aggiornaTotale() {
     let saldo = parseFloat(localStorage.getItem('conto' + conto)) //prendi saldo presente sul conto
     if (saldo - importo < 0) {
         alert("Operazione non possibile. Saldo insufficiente!")
+    } else if (saldo.isNan()) {
+        alert("Operazione non possibile.")
     } else {
         alert("Operazione andata a buon fine!")
         saldo -= importo
